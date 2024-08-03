20980339fef15ce0a184613b6a78c4eedc8c04a4
Matteo Moscone <matteo.moscone.1atgiorgi@gmail.com>
2024-08-03 14:52:07 +0200

    vdf

--- storico.php
+++ storico.php
@@ -1,21 +1,23 @@
 <?php
 $user = $_SESSION['username'];
+
 if (isset($_GET['squadra'])) {
     $squadra = $_GET['squadra'];
-    $query = "SELECT * FROM competizioni WHERE utente = ?";
+    $query = "SELECT nome, squadre FROM competizioni WHERE utente = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("s", $user);
     $stmt->execute();
     $result = $stmt->get_result();
     $stmt->close();
+
     while ($row = $result->fetch_assoc()) {
         $nome = $row['nome'];
-        $squadre = $row['squadre'];
-    }
-    $squadre = explode(",", $squadre);
-    for ($i = 0; $i < count($nome); $i++) {
+        $squadre = explode(",", $row['squadre']);
+
         if (in_array($squadra, $squadre)) {
             echo "ciao<br>";
         }
     }
 }
+?>
+
